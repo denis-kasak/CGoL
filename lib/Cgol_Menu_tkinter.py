c45ed82e6fb58a1288ceb75d0689f1541c85f539
denis-kasak <91327614+denis-kasak@users.noreply.github.com>
2022-10-23 05:11:54 +0200

    Pausenmenü Code Cleanup

--- lib/Cgol_Menu_tkinter.py
+++ lib/Cgol_Menu_tkinter.py
@@ -10,35 +10,35 @@ class Display:
         Output: Kein Output
         Besonders: Keine Besonderheiten
         """
-        self.master = Tk()
+        master = Tk()
 
-        self.master.geometry("200x100")
-        self.master.resizable(False, False)
+        master.geometry("200x100")
+        master.resizable(False, False)
 
-        self.master.title("CgoL")
+        master.title("CgoL")
 
-        self.title_label = Label(self.master, text="Spielmenü")
-        self.title_label.grid(row=0, column=0, sticky='ew')
+        title_label = Label(master, text="Spielmenü")
+        title_label.grid(row=0, column=0, sticky='ew')
 
-        self.save_button = Button(self.master, text="Speichern",
+        save_button = Button(master, text="Speichern",
                                   command=lambda: self.save_file(self.game.get_points(), False))
-        self.save_button.grid(row=1, column=0, sticky='ew')
+        save_button.grid(row=1, column=0, sticky='ew')
 
-        self.load_button = Button(self.master, text="Laden", command=lambda: self.open_saved_board())
-        self.load_button.grid(row=1, column=1, sticky='ew')
+        load_button = Button(master, text="Laden", command=lambda: self.open_saved_board())
+        load_button.grid(row=1, column=1, sticky='ew')
 
-        self.import_button = Button(self.master, text="Objekte laden", command=lambda: self.import_premade())
-        self.import_button.grid(row=2, column=0, sticky="ew")
+        import_button = Button(master, text="Objekte laden", command=lambda: self.import_premade())
+        import_button.grid(row=2, column=0, sticky="ew")
 
-        self.manual_button = Button(self.master, text="Anleitung")
-        self.manual_button.grid(row=2, column=1, sticky='ew')
+        manual_button = Button(master, text="Anleitung")
+        manual_button.grid(row=2, column=1, sticky='ew')
 
-        self.quit_button = Button(self.master, text="Quit", command=lambda: self.spiel_verlassen())
-        self.quit_button.grid(row=3, column=0, sticky='ew')
+        quit_button = Button(master, text="Quit", command=lambda: self.spiel_verlassen())
+        quit_button.grid(row=3, column=0, sticky='ew')
 
-        self.master.columnconfigure(0, weight=5, uniform="commi")
-        self.master.columnconfigure(1, weight=5, uniform="commi")
-        self.master.mainloop()
+        master.columnconfigure(0, weight=5, uniform="commi")
+        master.columnconfigure(1, weight=5, uniform="commi")
+        master.mainloop()
 
 
 a = Display()
